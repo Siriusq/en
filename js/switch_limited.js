fb96d2d1d45db0924683cb3a7ee70314a9b9e3ce
Siriusq <eargon.skyle@gmail.com>
2022-06-07 16:15:46 +0800

    Site updated: 2022-06-07 16:15:45

--- js/switch_limited.js
+++ js/switch_limited.js
@@ -4,12 +4,12 @@ window.onload = function() {
     var switchLang = document.getElementsByClassName("menu-item menu-item-switch-to-chinese")[0];
     switchLang.onclick = function() {
         var href = window.location.href;
-        var indexOfEn = href.toLowerCase().indexOf('/en/about/'&'/en/tags/'&'/en/categories/'&'/en/archives/');
-        if(indexOfEn == -1) {
-            window.location.href = "https://siriusq.top";
+        var includesKeywords = href.includes("/en/about/") || href.includes("/en/tags/")|| href.includes("/en/categories/") || href.includes("/en/archives/");
+        if(includesKeywords) {
+            window.location.href = href.replace('/en/', '/');
         }
         else {
-            window.location.href = href.replace('/en/', '/');
+            window.location.href = "https://siriusq.top";
         }
         if(typeof(_prevOnload) === 'function') {
             _prevOnload();
