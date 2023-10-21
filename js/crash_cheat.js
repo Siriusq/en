9699646dbaff7c56773df57c74d21be8bf7bc711
Siriusq <eargon.skyle@gmail.com>
2023-10-21 23:16:14 +0800

    Site updated: 2023-10-21 23:16:12

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,16 +1,17 @@
- var OriginTitle = document.title;
- var titleTime;
- document.addEventListener('visibilitychange', function () {
-     if (document.hidden) {
-         $('[rel="icon"]').attr('href', "/images/crashed.png");
-         document.title = '╭(°A°`)╮ Aw, Snap!';
-         clearTimeout(titleTime);
-     }
-     else {
-         $('[rel="icon"]').attr('href', "/images/favicon.png");
-         document.title = '(ฅ>ω<*ฅ) Psych!  ' + OriginTitle;
-         titleTime = setTimeout(function () {
-             document.title = OriginTitle;
-         }, 2000);
-     }
- });
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        var icon = document.querySelector('[rel="icon"]');
+        icon.setAttribute('href', "/images/crashed.png");
+        document.title = '╭(°A°`)╮ Aw, Crashed!';
+        clearTimeout(titleTime);
+    } else {
+        var icon = document.querySelector('[rel="icon"]');
+        icon.setAttribute('href', "/images/favicon.png");
+        document.title = '(ฅ>ω<*ฅ) You got tricked!' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
