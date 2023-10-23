ef6e07b11333d7bde6c45fc2c5a6a8cac49bfeab
Siriusq <eargon.skyle@gmail.com>
2023-10-23 21:14:59 +0800

    Site updated: 2023-10-23 21:14:57

--- js/crash_cheat.js
+++ js/crash_cheat.js
@@ -1,15 +1,27 @@
 var OriginTitle = document.title;
 var titleTime;
+var crashedIcon;
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    crashedIcon = "/images/crashed-dark.png";
+} else {
+    crashedIcon = "/images/crashed.png";
+}
+
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
-        var icon = document.querySelector('[rel="icon"]');
-        icon.setAttribute('href', "/images/crashed.png");
-        document.title = '╭(°A°`)╮ Aw, Crashed!';
+        var icons = document.querySelectorAll("link[rel='icon']");
+        for (var i = 0; i < icons.length; i++) {
+            icons[i].href = crashedIcon;
+        }
+        document.title = '╭(°A°`)╮ Aw, Snap!';
         clearTimeout(titleTime);
     } else {
-        var icon = document.querySelector('[rel="icon"]');
-        icon.setAttribute('href', "/images/favicon.png");
-        document.title = '(ฅ>ω<*ฅ) You got tricked!' + OriginTitle;
+        var icons = document.querySelectorAll("link[rel='icon']");
+        for (var i = 0; i < icons.length; i++) {
+            icons[i].href = "/images/favicon.png";
+        }
+        document.title = '(ฅ>ω<*ฅ) You got tricked! ' + OriginTitle;
         titleTime = setTimeout(function () {
             document.title = OriginTitle;
         }, 2000);
